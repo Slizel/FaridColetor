edc6d72ab0b7aa43533a7a5343c2e3c8163e2311
sistemasfarid <ti.sistemas@faridnet.com.br>
2020-08-27 18:05:05 -0300

    leitura do arquivo txt no fragment

--- app/src/main/java/faridnet/com/faridcoletor/MainActivity.kt
+++ app/src/main/java/faridnet/com/faridcoletor/MainActivity.kt
@@ -1,27 +1,48 @@
 package faridnet.com.faridcoletor
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
-
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import faridnet.com.faridcoletor.Data.AppDatabase
 import kotlinx.android.synthetic.main.fragment_add.*
-import java.io.File
-import java.io.FileOutputStream
+import java.io.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+
+        private val PERMISSON_REQUEST_STORAGE = 1000
+        private val READ_REQUEST_CODE = 42
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //request permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                PERMISSON_REQUEST_STORAGE
+            )
+        }
+
         setupActionBarWithNavController(findNavController(R.id.fragment))
     }
 
@@ -36,13 +57,17 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId;
         if (id == R.id.add_action) {
             export()
             //Toast.makeText(this, "Exportado!", Toast.LENGTH_SHORT).show()
         } else if (id == R.id.add_action2) {
-            Import()
+            //readFileLineByLineUsingForEachLine("/mnt/sdcard/Download/PRODUTOS.txt")
+
+            performFileSearch()
+            //Import()
         }
 
         return super.onOptionsItemSelected(item)
@@ -81,19 +106,99 @@ class MainActivity : AppCompatActivity() {
         }.start()
     }
 
-    fun Import() {
 
-        var importIntent: Intent = Intent(Intent.ACTION_GET_CONTENT)
-        importIntent.setType("*/*")
-        startActivityForResult(importIntent, 10)
+    private fun readText(input: String): String? {
+
+        val file = File(input)
+        val text = StringBuilder()
+        try {
+            val br = BufferedReader(FileReader(file))
+            var line: String?
+            while (br.readLine().also { line = it } != null) {
+                text.append(line)
+                text.append("\n")
+            }
+            br.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return text.toString()
+    }
+
+    //select file from storage
+    private fun performFileSearch() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = "text/*"
+        startActivityForResult(intent, READ_REQUEST_CODE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == READ_REQUEST_CODE && resultCode == RESULT_OK) {
+            if (data != null) {
 
-        if (requestCode == 10) {
-            val filePath = data?.data?.path
-            importView.setText(filePath)
+                val uri = data.data
+                var path = uri!!.path
+                path = path!!.substring(path.indexOf(":") + 1)
+                Toast.makeText(this, "" + path, Toast.LENGTH_LONG).show()
+                importView.setText(readText(path))
+            }
         }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSON_REQUEST_STORAGE) {
+            Toast.makeText(this, "Permission granted!", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "PErmission not granted", Toast.LENGTH_LONG).show()
+            finish()
+        }
+    }
+
+
+//    fun readFileText(fileName: String): String {
+//        return assets.open(fileName).bufferedReader().use { it.readText() }
+//    }
+
+//    fun Import() {
+//
+//
+//        var importIntent: Intent = Intent(Intent.ACTION_GET_CONTENT)
+//        importIntent.setType("*/*")
+//        startActivityForResult(importIntent, 10)
+//    }
+//
+//
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        if (requestCode == 10) {
+//            val filePath = File(data?.data?.path?.toUri()?.path.toString())
+//
+//
+//            if (filePath.exists()) {
+//                assets.open(filePath.toString()).bufferedReader().use { it.readText() }
+//
+//        }
+//    }
+
+
+//    fun readFileLineByLineUsingForEachLine(fileName: String) {
+//
+//        val lineList = mutableListOf<String>()
+//
+//        File(fileName).useLines { lines -> lines.forEach { lineList.add(it) }}
+//        lineList.forEach { println(">  " + it) }
+
+
 }
+
+
+
+
+
